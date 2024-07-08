9d6c4e8705b9bd5a813a46901c4e277bff2ce776
Manu Garcia <mgrmanu10@gmail.com>
2024-07-08 13:36:16 +0200

    v.1.0 ugbi

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
-# ubgi
-User Behaviour and Gaze Integrator
+# [UBGI]()
+
+## A Tool for Incorporating Eye Tracking Data in RPA: Enhancing User Behavior Logs
+
+Manuel García-Romero (orcid=0000-0003-2113-3497) (mgarcia44@us.es)
+Antonio Martínez-Rojas (orcid=0000-0002-2782-9893) (amrojas@us.es)
+A. Jiménez-Ramírez (orcid=0000-0001-8657-992X) (ajramirez@us.es)
+J. G. Enríquez (orcid=0000-0002-2631-5890) (jgenriquez@us.es)
+
+University of Seville. Computer Languages and Systems Department. E.T.S. Ingeniería Informática. Avenida Reina Mercedes, s/n, 41012, Seville, Spain}
+
+Proceedings of the Demonstration \& Resources Track, Best BPM Dissertation Award, and Doctoral Consortium at BPM 2024 colocated with the 22th International Conference on Business Process Management, BPM 2024, Krakow, Poland, September 1-6, 2024
+
+<br />
+
+## Demo
+
+> Create a new user on the **registration page** or executing in the terminal "python manage.py createsuperuser".
+
+> Recommendation -> User:demo;Password:demoBPM2024
+
+> The demo video is at 
+
+
+<br />
+
+## Quick start
+
+```bash
+$ # Get the code
+$ git clone https://github.com/RPA-US/ubgi.git
+$ cd ubgi
+```
+
+> After getting the code, open a terminal and navigate to the working directory, with product source code.
+> The following software is needed to run this platform if you want to install via local:
+- Python 3.10 or newer
+- Install PostgreSQL
+- C++ Dev Tools from Visual Studio: ![visual_studio_c++_features](apps\static\assets\img\image.png) 
+
+> You can install ugbi via docker if you do not want to install ugbi,python and PostgreSQL locally. 
+``` bash
+$ docker-compose -f docker-compose-dev.yml up
+```
+Remember to attach the container in VS code and install python for that container instance.
+
+
+```bash
+$ # Virtualenv modules installation (Unix based systems)
+$ virtualenv env
+$ source env/Scripts/activate
+$
+$ # Virtualenv modules installation (Windows based systems)
+$ # virtualenv env
+$ # .\env\Scripts\activate
+$
+$ # Install modules - SQLite Storage
+$ pip install -r requirements.txt
+$
+$ 
+```
+
+> Copy the content from ".env.sample" and paste it in a new ".env" . This ".env" must be placed in ubgi/core. Change the values if you need it
+
+```bash
+
+$ python manage.py makemigrations apps_analyzer apps_behaviourmonitoring apps_featureextraction apps_notification
+$ python manage.py migrate
+$
+$ 
+$ python manage.py createsuperuser 
+$
+$ # Start the application (development mode)
+$ python manage.py runserver # default port 8000
+$
+$ # Start the app - custom port
+$ # python manage.py runserver 0.0.0.0:<your_port>
+$
+$ # Access the web app in browser: http://127.0.0.1:8000/
+```
+
+> Note: To use the app, please access the registration page and create a new user. After authentication, the app will unlock the private pages.
+
+## Eye Tracking Use and Support
+
+To record a gaze log with the eye tracking software integrated into ScreenRPA (WebGazer.js <https://github.com/brownhci/WebGazer>), follow these steps:
+
+1. **Start the UGBI Web Application**
+   - Launch the UGBI web application.
+
+2. **Access WebGazer**
+   - Click on "Access to WebGazer" to open the WebGazer.js suite.
+
+3. **Prepare Your Case Study Scenario**
+   - Arrange all the applications you will interact with in windowed mode.
+   - Ensure the UGBI tab in your browser is open and in the background.
+
+4. **Start Recording**
+   - Begin recording the UI Log usingStepRecorders.
+   - Then, click on "Start" in the WebGazer suite to record the Gaze Log.
+
+5. **Perform Your Activity**
+   - Stay calm and perform your activity naturally. UGBI will handle the rest, recording both your UI log and Gaze Log!
+
+By following these steps, you will effectively utilize the eye tracking capabilities of UGBI to capture comprehensive logs of your interactions.
+
+
---
+++ apps/featureextraction/SOM/screen2som/utils.py
@@ -0,0 +1,214 @@
+import os
+import cv2
+import numpy as np
+from shapely.geometry import Polygon, Point
+
+
+def detect_duplicates(detected_shapes):
+    # Create a 2d numpy array of shape (len(detected_shapes), len(detected_shapes))
+    duplicates = []
+    for i in range(len(detected_shapes)):
+        for j in range(i + 1, len(detected_shapes)):
+            if i == j:
+                continue
+            detected_polygon1 = Polygon(detected_shapes[i]["points"])
+            detected_polygon2 = Polygon(detected_shapes[j]["points"])
+            intersection = detected_polygon1.intersection(detected_polygon2).area
+            union = detected_polygon1.union(detected_polygon2).area
+            if intersection / union >= 0.5:
+                duplicates.append((detected_polygon1, detected_polygon2))
+
+    return duplicates
+
+
+def save_bordered_images(img_path, detected_shapes, path_to_save_bordered_images):
+    # Give a distinct different color to each label
+    tint_colors = {
+        "Application": (32, 200, 255),
+        "Dock": (100, 33, 30),
+        "Taskbar": (210, 45, 215),
+        "Header": (55, 155, 255),
+        "Scrollbar": (0, 123, 50),
+        "Toolbar": (255, 111, 94),
+        "BrowserToolbar": (180, 0, 200),
+        "TabActive": (75, 0, 130),
+        "TabInactive": (238, 130, 238),
+        "Sidebar": (255, 0, 0),
+        "Navbar": (0, 255, 0),
+        "Container": (0, 0, 255),
+        "Image": (255, 255, 0),
+        "BrowserURLInput": (0, 255, 255),
+        "WebIcon": (255, 0, 255),
+        "Icon": (192, 192, 192),
+        "Switch": (128, 128, 128),
+        "BtnSq": (128, 0, 0),
+        "BtnPill": (128, 128, 0),
+        "BtnCirc": (0, 128, 0),
+        "CheckboxChecked": (128, 0, 128),
+        "CheckboxUnchecked": (0, 128, 128),
+        "RadiobtnSelected": (0, 0, 128),
+        "RadiobtnUnselected": (255, 165, 0),
+        "TextInput": (255, 20, 147),
+        "Dropdown": (220, 20, 60),
+        "Link": (50, 205, 50),
+        "Text": (70, 130, 180),
+    }
+
+    img = cv2.imread(img_path)
+    for i in range(len(detected_shapes)):
+        # Draw Polygons 
+        cv2.polylines(
+            img,
+            np.int32([detected_shapes[i]["points"]]),
+            True,
+            tint_colors[detected_shapes[i]["class"]],
+            2,
+        )
+
+        img_aux = img.copy()
+        cv2.fillPoly(
+            img_aux,
+            np.int32([detected_shapes[i]["points"]]),
+            tint_colors[detected_shapes[i]["class"]],
+        )
+
+        img = cv2.addWeighted(img_aux, 0.2, img, 0.8, 0)
+
+    cv2.imwrite(os.path.join(path_to_save_bordered_images, os.path.basename(img_path) + "_bordered.png"), img)
+
+def coco_to_compos(coco_anns, type="bbox", id_start=1):
+    res = []
+    for i, ann in enumerate(coco_anns):
+        if type == "bbox":
+            x, y, w, h = ann["bbox"]
+            points = [
+                [x, y],
+                [x + w, y],
+                [x + w, y + h],
+                [x, y + h],
+            ]
+
+            if Polygon(points).is_valid == False:
+                continue
+
+            res.append(
+                {
+                    "class": ann["category_name"],
+                    "text": "", #TODO,
+                    "points": points,
+                    "centroid": list(Polygon(points).centroid.coords[0])
+                }
+            )
+
+        elif type == "seg":
+            points = np.array(ann["segmentation"][0]).reshape(-1, 2)
+
+            if Polygon(points).is_valid == False:
+                continue
+
+            res.append(
+                {
+                    "class": ann["category_name"],
+                    "text": "", #TODO,
+                    "points": points.tolist(),
+                    "centroid": list(Polygon(points).centroid.coords[0])
+                }
+            )
+        else:
+            raise ValueError("Invalid type. Valid types are 'bbox' and 'seg'")
+
+        for i, shape in enumerate(res):
+            shape["id"] = i + id_start
+    
+    return res
+
+
+
+def json_inference_to_compos(anns, type="bbox", id_start=1):
+    res = []
+    for i, ann in enumerate(anns):
+        if type == "bbox":
+            box = ann["box"]
+
+            points = [
+                [box["x1"], box["y1"]],
+                [box["x2"], box["y1"]],
+                [box["x2"], box["y2"]],
+                [box["x1"], box["y2"]],
+            ]
+
+            if Polygon(points).is_valid == False:
+                continue
+
+            res.append(
+                {
+                    "class": ann["name"],
+                    "text": "", #TODO,
+                    "points": points,
+                    "centroid": list(Polygon(points).centroid.coords[0])
+                }
+            )
+
+        elif type == "seg":
+            x_points = ann["segments"]["x"]
+            y_points = ann["segments"]["y"]
+            points = np.array([x_points, y_points]).T
+
+            if Polygon(points).is_valid == False:
+                continue
+
+            res.append(
+                {
+                    "class": ann["name"],
+                    "text": "", #TODO,
+                    "points": points.tolist(),
+                    "centroid": list(Polygon(points).centroid.coords[0])
+                }
+            )
+        else:
+            raise ValueError("Invalid type. Valid types are 'bbox' and 'seg'")
+        
+        for i, shape in enumerate(res):
+            shape["id"] = i + id_start
+
+    return res
+
+def merge_text_with_OCR(detections, img_index, text_detected_by_OCR):
+    # Store on global_y all the "y" coordinates and text boxes
+    # Each row is a different text box, much more friendly than the format returned by keras_ocr 
+    global_y = []
+    global_x = []
+    words = {}
+    words[img_index] = {}
+
+    if len(text_detected_by_OCR) > 0:
+        for j in range(0, len(text_detected_by_OCR[img_index])):
+            coordenada_y = []
+            coordenada_x = []
+
+            for i in range(0, len(text_detected_by_OCR[img_index][j][1])):
+                coordenada_y.append(text_detected_by_OCR[img_index][j][1][i][1])
+                coordenada_x.append(text_detected_by_OCR[img_index][j][1][i][0])
+
+            word = text_detected_by_OCR[img_index][j][0]
+            centroid = (np.mean(coordenada_x), np.mean(coordenada_y))
+            if word in words[img_index]:
+                words[img_index][word] += [centroid]
+            else:
+                words[img_index][word] = [centroid]
+
+            global_y.append(coordenada_y)
+            global_x.append(coordenada_x)
+            # print('Coord y, cuadro texto ' +str(j+1)+ str(global_y[j]))
+            # print('Coord x, cuadro texto ' +str(j+1)+ str(global_x[j]))
+
+    for i, compo in enumerate(detections["compos"]):
+        if compo["class"] == "Text":
+            for word in words[img_index]:
+                for centroid in words[img_index][word]:
+                    if Polygon(compo["points"]).contains(Point([centroid])):
+                        # Add word to the text box
+                        if compo["text"] == "":
+                            compo["text"] = word
+                        else:
+                            compo["text"] += " " + word
